556ef998d0714870b2ecfd158db275f3dae35f3a
Jemish Italiya <jemishitaliya@Jemishs-MacBook-Air.local>
2025-07-04 18:17:21 -0700

    Request Page and card status page added

---
+++ pages/cardstatus/cardstatus.vue
@@ -0,0 +1,137 @@
+<template>
+  <view class="card-status">
+    <view class="button-container"
+      ><img src="/static/icons/back-button.png" class="back-button"
+    /></view>
+    <view class="title-text"> We’ve got your request! </view>
+    <view class="description">
+      Lorem ipsum dolor sit amet consectetur. Lorem nec ullamcorper aliquam id
+      dolor id volutpat mollis.
+    </view>
+    <view class="timeline">
+      <view v-for="(item, index) in events" :key="index" class="timeline-item">
+        <view class="dot" :class="{ active: index === 0 }"></view>
+        <view class="line" v-if="index !== events.length - 1"></view>
+        <view class="content">
+          <view
+            ><text class="title">{{ item.title }}</text></view
+          >
+          <view
+            ><text class="date">{{ item.date }}</text></view
+          >
+        </view>
+      </view>
+    </view>
+    <view><button class="submit-button">Got it</button> </view>
+  </view>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      events: [
+        { title: "Card is delivered.", date: "Sun. May 16, 2024, 12:06 PM" },
+        {
+          title: "Shipping Label Created",
+          date: "Sun. May 16, 2024, 12:06 PM",
+        },
+        { title: "Request Submitted", date: "Sun. May 16, 2024, 12:06 PM" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.title-text {
+  color: white;
+  font-size: 24px;
+  align-self: flex-start;
+  padding: 0px 15px;
+}
+.card-status {
+  background-color: #211134;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+}
+.description {
+  color: #bbbbbb;
+  align-self: flex-start;
+  padding: 0px 15px;
+}
+.timeline {
+  position: relative;
+  padding-left: 30rpx;
+  align-self: flex-start;
+  padding: 0px 15px;
+}
+
+.timeline-item {
+  position: relative;
+  margin-bottom: 60rpx;
+}
+
+.dot {
+  width: 16rpx;
+  height: 16rpx;
+  border-radius: 50%;
+  background-color: #ccc;
+  position: absolute;
+  left: 0;
+  top: 6rpx;
+  z-index: 1;
+}
+
+.dot.active {
+  background-color: #e940c3;
+}
+
+.line {
+  position: absolute;
+  width: 2rpx;
+  background-color: #888;
+  top: 20rpx;
+  bottom: -60rpx;
+  left: 7rpx;
+}
+
+.content {
+  padding-left: 30rpx;
+}
+
+.title {
+  font-size: 28rpx;
+  color: #ffffff;
+}
+
+.date {
+  font-size: 24rpx;
+  color: #aaaaaa;
+}
+.submit-button {
+  background: linear-gradient(90deg, #a44fff, #ff6e6e);
+  width: 354px;
+  height: 50px;
+  border: none;
+  border-radius: 12px;
+  color: white;
+  font-size: 18px;
+  margin-top: 20px;
+  cursor: pointer;
+  transition: opacity 0.3s ease;
+  justify-content: center;
+}
+.back-button {
+  height: 24px;
+  width: 24px;
+}
+.button-container {
+  align-self: flex-start;
+  padding: 0px 15px;
+  margin-bottom: 40px;
+}
+</style>
---
+++ pages/initiatearequest/initiatearequest.vue
@@ -0,0 +1,166 @@
+<template>
+  <view class="requestPage">
+    <view class="formContainer">
+      <view class="button-container"
+        ><img src="/static/icons/back-button.png" class="back-button"
+      /></view>
+      <view class="title-page">Initiate A Request</view>
+
+      <view class="form">
+        <view class="title">Cert Number</view>
+        <view>
+          <input
+            type="text"
+            class="input-field"
+            placeholder="Please Enter Cert Number"
+          />
+        </view>
+
+        <view class="title">Agency</view>
+        <view>
+          <select class="input-field">
+            <option class="select-option" value="" disabled selected hidden>
+              Please select Agency
+            </option>
+            <option class="select-option">select 2</option>
+            <option class="select-option">select 3</option>
+          </select>
+        </view>
+
+        <view class="title">Rank</view>
+        <view>
+          <select class="input-field">
+            <option class="select-option" value="" disabled selected hidden>
+              Please select Rank
+            </option>
+            <option class="select-option">select 2</option>
+            <option class="select-option">select 3</option>
+          </select>
+        </view>
+
+        <view class="title">IP Category</view>
+        <view>
+          <select class="input-field">
+            <option class="select-option" value="" disabled selected hidden>
+              Please select IP Category
+            </option>
+            <option class="select-option">select 2</option>
+            <option class="select-option">select 3</option>
+          </select>
+        </view>
+
+        <view>
+          <button class="submit" @click="goNext">Submit</button>
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+<script>
+export default {
+  components: {},
+  methods: {
+    goNext() {
+      uni.navigateTo({ url: "/pages/cardstatus/cardstatus" });
+    },
+  },
+};
+</script>
+
+<style>
+.requestPage {
+  background-color: #211134;
+  color: #a4a4a4;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: sans-serif;
+}
+
+.formContainer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.title-page {
+  font-size: 22px;
+  color: white;
+  margin-bottom: 20px;
+  align-self: flex-start;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  font-size: 15px;
+  align-items: center;
+}
+
+.title {
+  color: #a4a4a4;
+  align-self: flex-start;
+}
+
+.input-field {
+  background-color: #ffffff39;
+  box-sizing: border-box;
+  width: 354px;
+  height: 55px;
+  border: 1px solid #ffffff50;
+  padding: 0 15px;
+  border-radius: 12px;
+  color: #a4a4a4;
+  opacity: 80%;
+  font-size: 16px;
+  appearance: none;
+}
+
+.input-field:focus {
+  outline: none;
+  border-color: #a44fff;
+  box-shadow: 0 0 5px #a44fff66;
+}
+
+.input-field::placeholder {
+  color: #cccccc;
+  opacity: 1;
+  font-style: italic;
+}
+
+.select-option[disabled] {
+  color: #cccccc;
+}
+
+/* Submit button */
+.submit {
+  background: linear-gradient(90deg, #a44fff, #ff6e6e);
+  width: 354px;
+  height: 50px;
+  border: none;
+  border-radius: 12px;
+  color: white;
+  font-size: 18px;
+  margin-top: 20px;
+  cursor: pointer;
+  transition: opacity 0.3s ease;
+  justify-content: center;
+}
+
+.submit:hover {
+  opacity: 0.85;
+}
+input {
+  max-width: 354px;
+}
+.back-button {
+  height: 24px;
+  width: 24px;
+}
+.button-container {
+  align-self: flex-start;
+  margin-bottom: 40px;
+}
+</style>
